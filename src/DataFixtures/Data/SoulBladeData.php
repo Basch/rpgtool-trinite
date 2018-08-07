d64b41bbddfb2889096f2daefca672946e4abd03
Basch <ced_46000@hotmail.com>
2018-08-07 16:48:48 +0200

    Data pour Asset et FireBlade

---
+++ src/DataFixtures/Data/SoulBladeData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures\Data;
+
+
+class SoulBladeData
+{
+    public static $DATA = [
+        [
+            'id' => 1,
+            'name' => 'Jibr\'Il',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Izrâ\'Il',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Mickha\'Il',
+        ],
+        [
+            'id' => 4,
+            'name' => 'Isrâf\'Il',
+        ],
+        
+    ];
+}
