20143b0704092d61d5ef49e247d9acea28ca66f9
Basch <ced_46000@hotmail.com>
2018-08-07 20:04:32 +0200

    Entités pour Adam et Verse Data pour Adam et Verse

---
+++ src/DataFixtures/Data/AdamData.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cedric Molines
+ * Date: 2018-08-03
+ * Time: 12:08
+ */
+
+namespace App\DataFixtures\Data;
+
+
+class AdamData
+{
+    public static $DATA = [
+        [
+            'id' => 1,
+            'name' => 'Aaron'
+        ],
+         [
+            'id' => 2,
+            'name' => 'Alexandre'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Sémiramis'
+        ],
+        [
+            'id' => 4,
+            'name' => 'Simon'
+        ],
+        [
+            'id' => 5,
+            'name' => 'Hel'
+        ],
+        [
+            'id' => 6,
+            'name' => 'Attila'
+        ],
+        [
+            'id' => 7,
+            'name' => 'Cagliostro'
+        ],
+         [
+            'id' => 8,
+            'name' => 'Aleister Crowley'
+        ],
+
+    ];
+}
